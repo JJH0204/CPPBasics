b821f40b4bdff374426cce3f9525f224b0629277
JJH0204 <jaeho6627@kakao.com>
2024-05-16 17:54:29 +0900

    test build

--- build_mac.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p build/build_mac
-cd build/build_mac
-cmake ../../
-make
--- bulid_win.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-mkdir -p build/build_win
-cd build/build_win
-cmake -G "MinGW Makefiles" ../../
-cmake --build .
-pause
