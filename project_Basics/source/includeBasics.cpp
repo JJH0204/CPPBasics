d1670e65444db6634e2bdcad3380bbb2e112330c
JJH0204 <jaeho6627@kakao.com>
2024-05-18 14:16:12 +0900

    commit basics

--- project_Basics/source/includeBasics.cpp
+++ project_Basics/source/includeBasics.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include "includeBasics.h"
+using namespace std;
 
 void ex_main (void)
 {
     /* 변수 정의 및 데이터 저장 */
     int a = 100, b = 200, c = 300;
-
+    string str;
     /* console output의 약자, 전달 받은 값을 콘솔창에 출력 */
-    std::cout << "Hello Jaeho Jung.\nI'm your PC" << std::endl;
-
+    cout << "Please input your name: ";
+    cin >> str;
+    cout << "Hi " << str << "~!" << endl;
     /* 변수에 저장된 값 출력 */
-    std::cout << a << b << c << c << std::endl;
+    cout << a << b << c << c << std::endl;
 }
