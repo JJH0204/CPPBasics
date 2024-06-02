4ecb4868fd90f45a5a053cc2d0483910fb1be0cf
JJH0204 <jaeho6627@kakao.com>
2024-06-02 09:59:25 +0900

    file cleanup

---
+++ Tetris/include/Block.h
@@ -0,0 +1,9 @@
+#ifndef __BLOCK__
+#define __BLOCK__
+
+class Block
+{
+    
+};
+
+#endif
--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -1,10 +1,8 @@
 /* 테트리스 구현 프로젝트 */
-#include "Tetris.h"
+#include "Block.h"
 
 int main(int argc, char *argv[])
 {
-    Tetris::GameManager GameManager;
-
-    GameManager.update();
+    
     return 0;
 }
